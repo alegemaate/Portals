a204cbef997973396cc9e0b063fac074616473be
Allan Legemaate <alegemaate@gmail.com>
2014-12-24 12:39:10 -0500

    First Commit

---
+++ build/Basic.bat
@@ -0,0 +1,1 @@
+Portals.exe basic
---
+++ player.h
@@ -0,0 +1,48 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <allegro.h>
+#include "id.h"
+#include "tile.h"
+#include "tools.h"
+
+class player
+{
+  public:
+    player();
+    virtual ~player();
+
+    void load_assets();
+    void logic( tile newTiles[][32]);
+    void draw( BITMAP *tempBuffer);
+    void reset( int newStartX, int newStartY);
+    void setCoorinates( int newPortalCoordinates[][2][2]);
+
+    bool getWin();
+    bool getDead();
+
+    int getX();
+    int getY();
+  protected:
+
+  private:
+    // Variables
+    int x, y, jumping, jump_height, yVelocity, characterDir;
+    bool portalsActive, touchingPortal;
+    int portalCoordinates[4][2][2];
+
+    bool dead, win;
+
+    // Images
+    BITMAP *character[3];
+    BITMAP *cat;
+
+    // Block sounds
+    SAMPLE *crumble, *teleport, *boxslide, *electricity, *lever, *conveyorBelt, *wind, *splash;
+
+    // Character sounds
+    SAMPLE *fallLeaves, *fallWood, *oof;
+
+};
+
+#endif // PLAYER_H
